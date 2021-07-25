1727fb9946d387796cf8b58ec76f9754ed899e45
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-25 12:28:14 +0530

    added project files

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema],
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({
+  name: "Welcome.",
+});
+
+const array = [item1];
+
+app.get("/", function (req, res) {
+  Item.find((err, items) => {
+    if (items.length === 0) {
+      Item.insertMany(array);
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "My To-do's", newListItems: items });
+    }
+  });
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  const listName = req.body.list;
+
+  const newElement = new Item({
+    name: item,
+  });
+
+  if (listName === "My To-do's") {
+    newElement.save();
+    res.redirect("/");
+  } else {
+    List.findOne(
+      {
+        name: listName,
+      },
+      (err, list) => {
+        if (err) {
+          console.log(err);
+        } else {
+          list.items.push(newElement);
+          list.save();
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
+});
+
+app.post("/delete", function (req, res) {
+  const id = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "My To-do's") {
+    Item.findOneAndDelete({ _id: id }, (err) => {
+      if (!err) {
+        res.redirect("/");
+      } else {
+        console.log(err);
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      {
+        name: listName,
+      },
+      {
+        $pull: { items: { _id: id } },
+      },
+      (err) => {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
+});
+
+app.get("/:customName", (req, res) => {
+  const custom = _.capitalize(req.params.customName);
+
+  if (
+    List.findOne({ name: custom }, (err, list) => {
+      if (err) {
+        console.log(err);
+      } else if (list) {
+        res.render("list", { listTitle: list.name, newListItems: list.items });
+      } else {
+        const list = new List({
+          name: custom,
+          items: array,
+        });
+
+        list.save();
+        res.render("list", { listTitle: list.name, newListItems: list.items });
+      }
+    })
+  );
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
---
+++ explain.md
@@ -0,0 +1,8 @@
+- todolistdb contains two collections, named as Item and List by us.
+- MongoDB saves them as two collections, i.e:
+
+  - items
+  - lists
+
+- items collection contains objects as to-do string elements for the main list.
+- list collection contains objects with a listName and an array of to-do list items.
